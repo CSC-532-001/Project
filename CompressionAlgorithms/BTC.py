0a652e3161f708f9a6050e86d7259d1f0459704f
Kmoran1512 <kmoran.1512@gmail.com>
2024-05-01 16:40:04 -0400

    fixing BCT

--- CompressionAlgorithms/BTC.py
+++ CompressionAlgorithms/BTC.py
@@ -1,14 +1,15 @@
 import numpy as np
 from skimage.measure import block_reduce
 
+
 def btc_compress(img: np.ndarray, block=8):
-    if len(img.shape) == 2: 
+    if len(img.shape) == 2:
         reduced_image = block_reduce(img, (block, block), func=np.mean)
         expanded = np.repeat(np.repeat(reduced_image, block, axis=0), block, axis=1)
-        expanded = expanded[:img.shape[0], :img.shape[1]]
+        expanded = expanded[: img.shape[0], : img.shape[1]]
     else:
         reduced_image = block_reduce(img, (block, block, block), func=np.mean)
         expanded = np.repeat(np.repeat(reduced_image, block, axis=0), block, axis=1)
-        expanded = expanded[:img.shape[0], :img.shape[1], :img.shape[2]]
+        expanded = expanded[: img.shape[0], : img.shape[1], : img.shape[2]]
 
     return np.where(img > expanded, 1, 0)
--- CompressionAlgorithms/DWT.py
+++ CompressionAlgorithms/DWT.py
@@ -1,14 +1,18 @@
 import numpy as np
 import pywt
 
+
 def wavelet_transform(img, ratio=0.5):
     compressed = compress(img, ratio)
     return decompress_image(compressed)
 
+
 def compress(img, ratio):
     coeffs = pywt.wavedec2(img, "haar")
 
-    flattened_coeffs = np.concatenate([c.flatten() for level_coeffs in coeffs for c in level_coeffs])
+    flattened_coeffs = np.concatenate(
+        [c.flatten() for level_coeffs in coeffs for c in level_coeffs]
+    )
     sorted_indices = np.argsort(np.abs(flattened_coeffs))
 
     # Keep only the most important coefficients
@@ -25,15 +29,20 @@ def compress(img, ratio):
             if isinstance(c, tuple):
                 for sub_c in c:
                     flat_len = np.prod(sub_c.shape)
-                    level_compressed_coeffs.append(flattened_coeffs[idx : idx + flat_len].reshape(sub_c.shape))
+                    level_compressed_coeffs.append(
+                        flattened_coeffs[idx : idx + flat_len].reshape(sub_c.shape)
+                    )
                     idx += flat_len
             else:
                 flat_len = np.prod(c.shape)
-                level_compressed_coeffs.append(flattened_coeffs[idx : idx + flat_len].reshape(c.shape))
+                level_compressed_coeffs.append(
+                    flattened_coeffs[idx : idx + flat_len].reshape(c.shape)
+                )
                 idx += flat_len
         compressed_coeffs.append(level_compressed_coeffs)
 
     return compressed_coeffs
+
 
 def decompress_image(coeffs):
     return pywt.waverec2(coeffs, "haar")
